28bca57d279ebe379f8af319a78ea5fc26ac1d1c
Matthew Robertson <matthewrobertson03@gmail.com>
2012-06-07 14:52:17 -0700

    maybe working on ie

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ else if( $.browser.opera ) {
 	COLUMN_GAP_PROP = '-oColumnGap';
 }
 else if( $.browser.msie && parseInt($.browser.version, 10) >= 10 ) {
-	COLUMN_WIDTH_PROP = '-msColumnWidth';
-	COLUMN_GAP_PROP = '-msColumnGap';
+	COLUMN_WIDTH_PROP = 'columnWidth';
+	COLUMN_GAP_PROP = 'columnGap';
 }
 else {
 	COLUMN_WIDTH_PROP = 'columnWidth';
